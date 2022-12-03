d177cdfbffb9f7487575b759f6c6c153cf1d5801
Nissim Chekroun <nissim.chekroun@gmail.com>
2022-12-03 07:37:41 +0100

    chore(day1): improve error handling

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,29 +1,48 @@
-use std::num::ParseIntError;
+use std::{io, num::ParseIntError};
 
-use aoc::{read_lines, AocError};
+use aoc::read_lines;
 
-fn main() -> Result<(), AocError> {
-    println!("Part 1: {}", part1()?);
-    println!("Part 2: {:?}", part2()?);
+#[derive(Debug)]
+enum Day1Error {
+    IoError(io::Error),
+    ParseIntError(ParseIntError),
+    EmptyInput,
+}
 
-    Ok(())
+impl From<io::Error> for Day1Error {
+    fn from(e: io::Error) -> Self {
+        Self::IoError(e)
+    }
+}
+
+impl From<ParseIntError> for Day1Error {
+    fn from(e: ParseIntError) -> Self {
+        Self::ParseIntError(e)
+    }
 }
 
 const INPUT_PATH: &str = "inputs/day1.txt";
 
-fn part1() -> Result<u32, AocError> {
-    let lines = read_lines(INPUT_PATH)?;
-    let elf_calories = parse_elf_calories(lines)?;
+fn main() -> Result<(), Day1Error> {
+    let input = read_lines(INPUT_PATH)?;
+
+    println!("Part 1: {:?}", part1(&input)?);
+    println!("Part 2: {:?}", part2(&input)?);
+
+    Ok(())
+}
+
+fn part1(input: &Vec<String>) -> Result<u32, Day1Error> {
+    let elf_calories = parse_elf_calories(input)?;
 
     let elf_total_calories: Vec<u32> = elf_calories.into_iter().map(|v| v.iter().sum()).collect();
     let max_calories = elf_total_calories.into_iter().max();
 
-    Ok(max_calories.unwrap())
+    max_calories.ok_or(Day1Error::EmptyInput)
 }
 
-fn part2() -> Result<u32, AocError> {
-    let lines = read_lines(INPUT_PATH)?;
-    let elf_calories = parse_elf_calories(lines)?;
+fn part2(input: &Vec<String>) -> Result<u32, Day1Error> {
+    let elf_calories = parse_elf_calories(input)?;
 
     let mut elf_total_calories: Vec<u32> =
         elf_calories.into_iter().map(|v| v.iter().sum()).collect();
@@ -33,7 +52,7 @@ fn part2() -> Result<u32, AocError> {
     Ok(elf_total_calories[0..3].iter().sum())
 }
 
-fn parse_elf_calories(lines: Vec<String>) -> Result<Vec<Vec<u32>>, ParseIntError> {
+fn parse_elf_calories(lines: &Vec<String>) -> Result<Vec<Vec<u32>>, ParseIntError> {
     lines
         .split(|line| line.is_empty())
         .map(|v| v.iter().map(|s| s.parse()).collect())
