bcf91e288f483eb32b91c802903b988a32959041
Nissim Chekroun <nissim.chekroun@gmail.com>
2022-12-14 14:36:31 +0000

    chore: add template with unit tests

---
+++ src/bin/template.rs
@@ -0,0 +1,57 @@
+use std::io;
+
+use aoc::read_lines;
+
+#[derive(Debug)]
+enum Day0Error {
+    IoError(io::Error),
+}
+
+impl From<io::Error> for Day0Error {
+    fn from(e: io::Error) -> Self {
+        Self::IoError(e)
+    }
+}
+
+const INPUT_PATH: &str = "inputs/day0.txt";
+
+fn main() -> Result<(), Day0Error> {
+    let input = read_lines(INPUT_PATH)?;
+
+    println!("Part 1: {:?}", part1(&input)?);
+    println!("Part 2: {:?}", part2(&input)?);
+
+    Ok(())
+}
+
+fn part1(input: &Vec<String>) -> Result<usize, Day0Error> {
+    Ok(0)
+}
+
+fn part2(input: &Vec<String>) -> Result<usize, Day0Error> {
+    Ok(0)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // Make sure to remove any extra indentation (otherwise it will be part of the string)
+    const EXAMPLE: &str = "\
+ABCD
+";
+
+    #[test]
+    fn test_part1() {
+        let input: Vec<String> = EXAMPLE.lines().map(|s| s.to_owned()).collect();
+
+        assert_eq!(part1(&input).unwrap(), 42);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input: Vec<String> = EXAMPLE.lines().map(|s| s.to_owned()).collect();
+
+        assert_eq!(part2(&input).unwrap(), 42);
+    }
+}
